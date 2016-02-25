37092156d7b2b83836a5c5955a8b18857b622fe1
Dmitry <dmitry@pereslegin.ru>
2016-02-26 02:20:57 +0300

    Sampler

--- src/Sampler.php
+++ src/Sampler.php
@@ -429,27 +429,27 @@ class Sampler
         $imagick->clear();
     }
 
-    private function standardDeviation(array $a, $sample = false)
+    private function standardDeviation(array $values, $sample = false)
     {
-        $n = count($a);
-        if ($n === 0) {
+        $count = count($values);
+        if ($count === 0) {
             trigger_error("The array has zero elements", E_USER_WARNING);
             return false;
         }
-        if ($sample && $n === 1) {
+        if ($sample && $count === 1) {
             trigger_error("The array has only 1 element", E_USER_WARNING);
             return false;
         }
-        $mean = array_sum($a) / $n;
+        $mean = array_sum($values) / $count;
         $carry = 0.0;
-        foreach ($a as $val) {
-            $d = ((double) $val) - $mean;
-            $carry += $d * $d;
+        foreach ($values as $val) {
+            $diff = ((double) $val) - $mean;
+            $carry += $diff * $diff;
         };
         if ($sample) {
-            --$n;
+            --$count;
         }
-        return sqrt($carry / $n);
+        return sqrt($carry / $count);
     }
 
     /**
