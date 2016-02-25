885961af504ec1a09277ba9a2543113aaf60513a
Dmitry <dmitry@pereslegin.ru>
2016-02-26 02:27:42 +0300

    Request bug fix

--- src/Storage/Request.php
+++ src/Storage/Request.php
@@ -48,11 +48,11 @@ class Request
         foreach ($options as $key => $value) {
             $method = 'set' . ucfirst($key);
 
-            if (method_exists($this, $method)) {
-                $this->$method($value);
-            } else {
-                raise("Unexpected option '$key'");
+            if (!method_exists($this, $method)) {
+                $this->raise("Unexpected option '$key'");
             }
+            
+            $this->$method($value);
         }
 
         return $this;
